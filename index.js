bc71c713f8a590a0bbb0f91ad488571902eee638
CyberAri <arianobarros18@gmail.com>
2023-06-09 20:22:17 -0300

    Merge branch 'main' of https://github.com/CyberAri/Landing-Page_Aquitetura
    atualizando a landing page

---
+++ index.js
@@ -0,0 +1,14 @@
+var imgProjeto = window.document.getElementById("imgProjeto")
+
+function verOutraFoto(){
+    imgProjeto.src = "/imagens/foto2.svg.svg"
+}
+
+function verEssaFoto(){
+    imgProjeto.src = "/imagens/foto-projeto.svg"
+}
+
+
+
+
+
